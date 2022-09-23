f9def185cbdbe916008d8a674253c9a7e53124ab
luciejachimova <107799001+luciejachimova@users.noreply.github.com>
2022-09-23 13:27:55 +0200

    deleted the react logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from "./logo.svg";
 import "./App.css";
-
 export default function App() {
   return (
     <div className="App">
@@ -8,5 +6,4 @@ export default function App() {
     </div>
   );
 }
-
-App;
+App();
