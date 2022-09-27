f2b3bd0109e780f41ebaa40841b925e8d8933c6b
luciejachimova <107799001+luciejachimova@users.noreply.github.com>
2022-09-27 11:20:25 +0200

    add api documentation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,8 +12,9 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+
+    //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
