c5b4cff8044b4e36405a7ae6b09d825489cb1545
luciejachimova <107799001+luciejachimova@users.noreply.github.com>
2022-09-28 11:16:57 +0200

    Added phonetic

---
+++ src/Phonetic.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      {props.phonetic.audio ? (
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          Listen
+          <i class="fa-solid fa-volume-high"></i>
+        </a>
+      ) : null}
+      <span className="text">{props.phonetic.text} </span>
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-          
+        <section className="word">
+          <h2>{props.results.word}</h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <span key={index}>
+                <Phonetic phonetic={phonetic} />
+              </span>
+            );
+          })}
+        </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <section key={index}>
