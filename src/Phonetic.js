01ae9c0dbfdfa96493ccccbc439d5d95bb3ef7dd
luciejachimova <107799001+luciejachimova@users.noreply.github.com>
2022-10-03 10:54:52 +0200

    remowed noreferrer

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       {props.phonetic.audio ? (
-        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        <a href={props.phonetic.audio} target="_blank">
           Listen
           <i class="fa-solid fa-volume-high"></i>
         </a>
